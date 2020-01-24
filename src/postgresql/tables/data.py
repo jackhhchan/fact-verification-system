3c384d8d5051931b0b6d84b6f6540155c3fe4f2e
Jack Chan <hhc.jack.chan@gmail.com>
2020-01-24 23:44:38 +1100

    ADD postgresql.tables.data
    Added Data Table class.

---
+++ src/postgresql/tables/data.py
@@ -0,0 +1,18 @@
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.types import SMALLINT
+
+from . import Base
+
+
+class Data(Base):
+    __tablename__ = 'data'
+    
+    id = Column(Integer, primary_key=True)
+    page_id = Column(String)
+    sent_id = Column(SMALLINT)
+    sentence = Column(String)
+
+    def __repr__(self):
+        return "<Data(page_id={}, sent_id={}, sentence={})".format(
+            self.page_id, self.sent_id, self.sentence
+        )
