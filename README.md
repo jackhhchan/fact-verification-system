b765a43b090bccba6acfa5e46133d38b2e4839de
Jack Chan <hhc.jack.chan@gmail.com>
2020-02-05 15:51:06 +1100

    ADD db_query.query_sentence

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # watson-junior-2
+
+## src
+## src/postgres
+#### database storing raw dataset files
+#### 
+
+
+## src/fact_verification_system/classifier
+#### classifier to be run on its own container.
+
+## src/fact_verification_system/search
+#### search engine to retrieve relevant data.
--- src/postgres/db_query.py
+++ src/postgres/db_query.py
@@ -1,4 +1,7 @@
 from sqlalchemy.orm.session import Session
+from sqlalchemy.orm.exc import NoResultFound, MultipleResultsFound
+
+from postgres.tables.data import Data
 
 class DatabaseQuery(object):
     """ Handles Database Queries
@@ -10,3 +13,24 @@ class DatabaseQuery(object):
     @staticmethod
     def query_template(session:Session, **args) -> None:
         return session.query()
+
+    @staticmethod
+    def query_sentence(session: Session, page_id:str, sent_idx:int) -> str:
+        """ Returns a single sentence for the specific page_id and sent_idx. """
+        try:
+            ev =  session.query(Data).\
+                filter(Data.page_id==page_id).\
+                filter(Data.sent_idx==sent_idx).one()
+            return ev.sentence
+        except NoResultFound:
+            print("[DBQ] No results found for page_id: {}, sent_idx: {}".format(
+                page_id, 
+                sent_idx
+            ))
+        except MultipleResultsFound:
+            print("[DBQ] Multiple results found for page_id: {}, sent_idx: {}".format(
+                page_id, 
+                sent_idx
+            ))
+        except Exception as e:
+            print("[DBQ] Query error: {}".format(e))
