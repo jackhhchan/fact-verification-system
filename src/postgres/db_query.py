d1ec1ab80d0d420febcfeedae95cced1108afcce
Jack <jackhhchan912@gmail.com>
2020-02-13 12:13:43 +1100

    ADD async query to postgres.db_query

--- src/postgres/db_query.py
+++ src/postgres/db_query.py
@@ -34,3 +34,24 @@ class DatabaseQuery(object):
             ))
         except Exception as e:
             print("[DBQ] Query error: {}".format(e))
+
+    @staticmethod
+    async def async_query_sentence(session: Session, page_id:str, sent_idx:int) -> str:
+        """ Returns a single sentence for the specific page_id and sent_idx. """
+        try:
+            ev =  session.query(Wiki).\
+                filter(Wiki.page_id==page_id).\
+                filter(Wiki.sent_idx==sent_idx).one()
+            return ev.sentence
+        except NoResultFound:
+            print("[DBQ] No results found for page_id: {}, sent_idx: {}".format(
+                page_id, 
+                sent_idx
+            ))
+        except MultipleResultsFound:
+            print("[DBQ] Multiple results found for page_id: {}, sent_idx: {}".format(
+                page_id, 
+                sent_idx
+            ))
+        except Exception as e:
+            print("[DBQ] Query error: {}".format(e))
