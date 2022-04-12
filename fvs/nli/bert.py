2b78046fb405606d5629148b4f3207939f51fe5a
Jack Chan <hhc.jack.chan@gmail.com>
2022-04-12 19:03:38 +1000

    feat(bert, train): Add CLS token, Fix token type id - bert tokenizer, Add punctuation removal

--- fvs/nli/bert.py
+++ fvs/nli/bert.py
@@ -23,6 +23,11 @@ class BertNliConfig(object):
         self.num_embedding_layers = num_embedding_layers
         self.pool_method = pool_method
 
+    def __str__(self):
+        return f"(BertNliConfig nsp={self.nsp} \
+        num_embed_layers={self.num_embedding_layers} \
+        pool_method={self.pool_method})"
+
 
 class BERTNli(nn.Module):
     def __init__(self, config: BertNliConfig):
@@ -32,55 +37,75 @@ class BERTNli(nn.Module):
         self.tokenizer = BertTokenizer.from_pretrained("bert-base-uncased")
         for param in self.bert.parameters():
             param.requires_grad = False
-        self.bn1 = nn.BatchNorm1d(1536)
-        self.fc1 = nn.Linear(1536, 768)
-        self.bn2 = nn.BatchNorm1d(768)
-        self.relu1 = nn.LeakyReLU()
-        self.dropout1 = nn.Dropout(0.25)
-        self.fc2 = nn.Linear(768, 256)
-        self.bn3 = nn.BatchNorm1d(256)
-        self.relu2 = nn.LeakyReLU()
-        self.fc3 = nn.Linear(256, 1)
-        self.sigmoid = nn.Sigmoid()
+        # self.bn1 = nn.BatchNorm1d(1536)
+        # self.fc1 = nn.Linear(1536, 768)
+        # self.bn2 = nn.BatchNorm1d(768)
+        # self.relu1 = nn.LeakyReLU()
+        # self.dropout1 = nn.Dropout(0.25)
+        if self.config.nsp:
+            self.fc2 = nn.Linear(768, 256)
+            # self.bn3 = nn.BatchNorm1d(256)
+            self.relu2 = nn.LeakyReLU()
+            self.fc3 = nn.Linear(256, 1)
+            # self.sigmoid = nn.Sigmoid()
+
+        # self.fc_nsp = nn.Linear(768, 1)     # NSP
+        if not self.config.nsp:
+            self.fc_cat = nn.Linear(2 * 768, 1)  # Concatenated form
 
         self.bert_max_layer = 12  # bert-base
 
     def forward(self, claims, evidence):
-        claims = tokenizer(claims, return_tensors="pt", padding=True)
-        for k in claims.keys():
-            claims[k] = claims.get(k).to(device)
-        claims = self.bert(**claims)
-        claims = claims.last_hidden_state[:, 0, :]
-        claims.requires_grad = True
-        evidence = tokenizer(evidence, return_tensors="pt", padding=True)
-        for k in evidence.keys():
-            evidence[k] = evidence.get(k).to(device)
-        evidence = self.bert(**evidence)
-        evidence = evidence.last_hidden_state[:, 0, :]
-        evidence.requires_grad = True
-
-        x = torch.cat((claims, evidence), axis=1)
-        x = self.relu1(self.fc1(self.bn1(x)))
-        x = self.dropout1(x)
-        x = self.relu2(self.fc2(self.bn2(x)))
+        # claims = tokenizer(claims, return_tensors="pt", padding=True)
+        # for k in claims.keys():
+        #     claims[k] = claims.get(k).to(device)
+        # claims = self.bert(**claims)
+        # claims = claims.last_hidden_state[:, 0, :]
+        # claims.requires_grad = True
+        # evidence = tokenizer(evidence, return_tensors="pt", padding=True)
+        # for k in evidence.keys():
+        #     evidence[k] = evidence.get(k).to(device)
+        # evidence = self.bert(**evidence)
+        # evidence = evidence.last_hidden_state[:, 0, :]
+        # evidence.requires_grad = True
+        #
+        # x = torch.cat((claims, evidence), axis=1)
+        # x = self.relu1(self.fc1(self.bn1(x)))
         # x = self.dropout1(x)
-        x = self.fc3(self.bn3(x))
-        # x = self.sigmoid(x)       # replaced with BCELossWithLogits
+        # x = self.relu2(self.fc2(self.bn2(x)))
+        # # x = self.dropout1(x)
+        # x = self.fc3(self.bn3(x))
+        # # x = self.sigmoid(x)       # replaced with BCELossWithLogits
+
+        x = self.bert_outputs(claims, evidence).to(device)
+        # x.requires_grad = True
+        if self.config.nsp:
+            # x = self.fc_nsp(x)
+            x = self.fc3(self.relu2(self.fc2(x)))
+        else:
+            x = self.fc_cat(x)
         return x
 
     def bert_outputs(self, claims, evidence):
         """ Transforms text into BERT inputs. """
         if self.config.nsp:
             # leverage bert next sentence prediction training -- combine the two sentences separated by [SEP]
-            combined = [c + ' [SEP] ' + e for c in claims for e in evidence]
-            tokenised = self.tokenizer(combined, return_tensors='pt', padding=True)
-            embeddings = self.pool_layer_embeddings(tokenised)
-            embeddings = self.pool_token_embeddings(embeddings)
+            # combined = [claims[i] + ' [SEP] ' + evidence[i] for i in range(len(claims))]
+            tokenised = self.tokenizer(claims, evidence, return_tensors='pt', padding=True).to(device)
+            if self.config.pool_method is None or self.config.pool_method == 'cls':
+                embeddings = self.pool_layer_embeddings(tokenised)
+                embeddings = self.cls_embeddings(embeddings)
+            elif self.config.pool_method == 'pooler_output':
+                outputs = self.bert(**tokenised)
+                embeddings = outputs.get('pooler_output')
+            else:
+                embeddings = self.pool_layer_embeddings(tokenised)
+                embeddings = self.pool_token_embeddings(embeddings)
             return embeddings
         else:
             # get the sentence embedding for both and concat them
-            claims = self.tokenizer(claims, return_tensors='pt', padding=True)
-            evidence = self.tokenizer(evidence, return_tensors='pt', padding=True)
+            claims = self.tokenizer(claims, return_tensors='pt', padding=True).to(device)
+            evidence = self.tokenizer(evidence, return_tensors='pt', padding=True).to(device)
             claims = self.pool_layer_embeddings(claims)
             evidence = self.pool_layer_embeddings(evidence)
             claims = self.pool_token_embeddings(claims)
@@ -96,7 +121,7 @@ class BERTNli(nn.Module):
         hidden_states: Tuple[torch.Tensor] = outputs.get('hidden_states')
 
         start_from = self.bert_max_layer - self.config.num_embedding_layers
-        sum = torch.zeros(hidden_states[0].shape)
+        sum = torch.zeros(hidden_states[0].shape).to(device)
         for idx in range(start_from, self.bert_max_layer + 1):
             sum += hidden_states[idx]
         return sum
@@ -114,21 +139,63 @@ class BERTNli(nn.Module):
         else:
             return torch.sum(batch_token_embeddings, dim=1) / num_tokens
 
+    def cls_embeddings(self, batch_token_embeddings: torch.Tensor):
+        return batch_token_embeddings[:, 0, :]
 
-def config_train():
-    return [(optim.SGD(lr=0.01)),
-            (optim.SGD(lr=0.01, momentum=0.9)),
-            (optim.SGD(lr=0.001)),
-            (optim.SGD(lr=0.001, momentum=0.9)),
-            (optim.Adam(lr=0.01)),
-            (optim.Adam(lr=0.001))]
+
+def config_train(model):
+    optims = [
+        optim.SGD(lr=0.001, params=model.parameters()),
+        optim.SGD(lr=0.01, params=model.parameters()),
+        optim.SGD(lr=0.01, momentum=0.9, params=model.parameters()),
+        optim.SGD(lr=0.001, momentum=0.9, params=model.parameters()),
+        optim.Adam(lr=0.01, params=model.parameters()),
+        optim.Adam(lr=0.001, params=model.parameters()),
+        optim.AdamW(lr=2e-5, eps=1e-6, params=model.parameters()),
+    ]
+
+
+def config_optimisers():
+    return [
+        (optim.SGD, {'lr': 0.01}),
+        # (optim.SGD, {'lr': 0.001}),
+        # (optim.SGD, {'lr': 0.01, 'momentum': 0.9}),
+        (optim.SGD, {'lr': 0.001, 'momentum': 0.9}),
+        # (optim.Adam, {'lr': 0.01}),
+        (optim.Adam, {'lr': 0.001}),
+    ]
+
+
+def config_model():
+    return [
+        # BertNliConfig(nsp=True, num_embedding_layers=0, pool_method='avg'),  # 0 = last hidden layer only
+        # BertNliConfig(nsp=True, num_embedding_layers=4, pool_method='avg'),
+        BertNliConfig(nsp=True, num_embedding_layers=0, pool_method='cls'),        # use cls
+        # BertNliConfig(nsp=True, num_embedding_layers=0, pool_method='pooler_output'),        # use pool
+        # BertNliConfig(nsp=True, num_embedding_layers=4, pool_method=''),        # use cls
+        # BertNliConfig(nsp=False, num_embedding_layers=0, pool_method='avg'),
+        # BertNliConfig(nsp=False, num_embedding_layers=4, pool_method='avg'),
+    ]
 
 
 if __name__ == '__main__':
-    sent_1 = "Hello there"
-    sent_2 = "Bye now friend"
+    sent_1 = ["Hello there", "hello there"]
+    sent_2 = ["Bye now friend", 'bye now friend']
     # check how BertTokenizer encodes the sentences
     tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
-    tokenised = tokenizer(sent_1)
-    print(tokenised)
-    print()
+    # tokenised = tokenizer(sent_1 + ' [SEP] ' + sent_2)
+    # print(tokenised)
+    # flag: bool = False
+    # for i in range(len(tokenised.get('input_ids'))):
+    #     if tokenised.get('input_ids')[i] == 102 or flag:
+    #         flag = True
+    #         tokenised['token_type_ids'][i] = 1
+    # print(tokenised)
+    tokenised_1 = tokenizer(sent_1, sent_2)
+    print(tokenised_1)
+    # tokenised_2 = tokenizer(sent_2)
+    # tokenised_inputs = tokenizer.build_inputs_with_special_tokens(tokenised_1.get('input_ids'), tokenised_2.get('input_ids'))
+    # tokenised_types = tokenizer.create_token_type_ids_from_sequences(tokenised_1.get('input_ids'), tokenised_2.get('input_ids'))
+    # print(tokenised_inputs, tokenised_types)
+    # print(tokenizer.convert_ids_to_tokens(tokenised_inputs))
+
--- fvs/nli/train.py
+++ fvs/nli/train.py
@@ -1,4 +1,4 @@
-from fvs.nli.bert import BERTNli
+from fvs.nli.bert import BERTNli, config_train, config_model, config_optimisers
 import torch
 import torch.nn as nn
 from transformers import BertModel, BertTokenizer, BertConfig
@@ -6,16 +6,41 @@ import torch.functional as F
 import pandas as pd
 from torch.utils.data import DataLoader, Dataset
 import torch.optim as optim
+from torch.utils.tensorboard import SummaryWriter
+from datetime import datetime
+import numpy as np
+from sklearn.model_selection import train_test_split
+
+device = torch.device('cuda:{}'.format(torch.cuda.current_device()))
+
+from string import punctuation
+import re
+
+pattern = re.compile(f"(-L.B-|-R.B-|[{punctuation}])")
+
+
+def clean(s: str):
+    return re.sub(' +', ' ', pattern.sub('', s).encode("ascii", "ignore").decode())
+
 
 if __name__ == '__main__':
     df_raw = pd.read_csv("./train.csv")
+    df_raw['claim'] = df_raw['claim'].apply(clean)
+    df_raw['evidence_sentence'] = df_raw['evidence_sentence'].apply(clean)
 
     # make balanced
-    supports = df_raw[df_raw['label'].str.contains("SUPPORT") == True].sample(10000)
-    refutes = df_raw[df_raw['label'].str.contains("REFUTE") == True].sample(10000)
+    to_sample = 10
+    supports = df_raw[df_raw['label'].str.contains("SUPPORT") == True].sample(to_sample)
+    refutes = df_raw[df_raw['label'].str.contains("REFUTE") == True].sample(to_sample)
     df = pd.concat([supports, refutes])
 
+    # split train test
+    train, test = train_test_split(df, test_size=0.2)
+    # train = supports
+    # test = supports.copy()
 
+
+    ## Data Loaders ##
     class Train(Dataset):
         def __init__(self, df: pd.DataFrame):
             self.df = df
@@ -31,56 +56,83 @@ if __name__ == '__main__':
             return claim, evidence, label
 
 
-    loader = DataLoader(Train(df), batch_size=8, shuffle=True)
+    loader = DataLoader(Train(train), batch_size=8, shuffle=True)
+    test_loader = DataLoader(Train(test), batch_size=8)
 
-    device = torch.device('cuda:{}'.format(torch.cuda.current_device()))
-    bnli = BERTNli(BertConfig())
-    bnli.to(device)  # puts model params on gpu
-
+    ## CRITERION ##
     # criterion = nn.BCELoss()
     criterion = nn.BCEWithLogitsLoss()
-    # optimiser = optim.SGD(params=bnli.parameters(), lr=0.01, momentum=0.9)
-    # optimiser = optim.SGD(params=bnli.parameters(), lr=0.001, momentum=0.9)
-    optimiser = optim.SGD(params=bnli.parameters(), lr=0.001)
-    # optimiser = optim.SGD(params=bnli.parameters(), lr=0.0001)
-    # optimiser = optim.Adam(params=bnli.parameters(), lr=0.001)
 
-    hparams = {
-        'batch_size': loader.batch_size,
-        'optim': str(optimiser),
-    }
+    model_configs = config_model()
 
-    from torch.utils.tensorboard import SummaryWriter
-    from datetime import datetime
+    EPOCH = 10000
+    modulo = 1
+    for config in model_configs:
+        for opt_fn, opt_params in config_optimisers():
+            bnli = BERTNli(config=config)
+            bnli.to(device)
 
-    run: str = str(datetime.now())
-    writer = SummaryWriter(log_dir=f"runs/{run}")
+            opt_params['params'] = bnli.parameters()
+            optimiser = opt_fn(**opt_params)
 
-    EPOCH = 50
-    total_loss = list()
-    for epoch in range(EPOCH):
-        running_loss = 0.0
-        for i, data in enumerate(loader):
-            claim, evi, label = data
-            output = bnli(claims=claim, evidence=evi)
-            label = label.view(-1, 1).float().to(device)
+            hparams = {
+                'model_config': str(config),
+                'optim': str(optimiser),
+                'batch_size': loader.batch_size,
+                'epochs': EPOCH
+            }
 
-            optimiser.zero_grad()
-            loss = criterion(output, label)
-            loss.backward()  # compute grads
-            optimiser.step()  # apply grads
+            run: str = f"{str(datetime.now())}"
+            writer = SummaryWriter(log_dir=f"runs/{run}")
 
-            running_loss += loss.item()
-            total_loss.append(loss.item())
-            if (i + 1) % 40 == 0:
-                print(f"[{epoch + 1}, {i + 1}] loss: {running_loss / 100}")
-                writer.add_scalar("Loss", running_loss / 100, (epoch + 1) * i)
-                running_loss = 0
+            print(f"Training started [{run}]:\n{config}\nEpoch={EPOCH}\n{optimiser}")
+            total_loss = list()
+            for epoch in range(EPOCH):
+                running_loss = 0.0
+                for i, data in enumerate(loader):
+                    claim, evi, label = data
+                    output = bnli(claims=claim, evidence=evi)
+                    label = label.view(-1, 1).float().to(device)
 
-    import numpy as np
+                    optimiser.zero_grad()
+                    loss = criterion(output, label)
+                    loss.backward()  # compute grads
+                    torch.nn.utils.clip_grad_norm_(bnli.parameters(), 1.0)
+                    optimiser.step()  # apply grads
 
-    metrics = {'avg_loss': np.mean(total_loss),
-               'max_loss': np.max(total_loss),
-               'min_loss': np.min(total_loss)}
-    writer.add_hparams(hparams, metrics)
-    print("Training complete.")
+                    running_loss += loss.item()
+                    total_loss.append(loss.item())
+
+                    if (i + 1) % modulo == 0:
+                        # TODO: evaluation
+                        with torch.no_grad():
+                            bnli.eval()
+                            wrong = 0.0
+                            total = 0
+                            for data in test_loader:
+                                claim, evi, label = data
+                                y_pred = bnli(claims=claim, evidence=evi)
+                                y_pred = torch.sigmoid(y_pred)
+                                print(y_pred, label)
+                                wrong += torch.sum(torch.abs(
+                                    torch.round(y_pred).detach().cpu().view(-1) - label.detach().cpu().view(-1)))
+                                total += len(y_pred)
+                                # print(f"Number of wrongs: {wrong} over {len(y_pred)}")
+                            test_acc = 1.0 - float(wrong / total)
+                            writer.add_scalar("Test Accuracy", test_acc, (epoch + 1) * i)
+                            bnli.train()
+
+                        params = list(bnli.parameters())
+                        print(f"[{epoch + 1}, {i + 1}] loss: {running_loss / modulo} test_acc: {test_acc}")
+                        writer.add_scalar("Loss", running_loss / modulo, (epoch + 1) * i)
+                        # writer.add_histogram("Linear0_weights", params[-4], (epoch + 1) * i)
+                        # writer.add_histogram("Linear0_grad", params[-4].grad.view(-1), (epoch + 1) * i)
+                        writer.add_histogram("Linear1_weights", params[-2], (epoch + 1) * i)
+                        writer.add_histogram("Linear1_grad", params[-2].grad.view(-1), (epoch + 1) * i)
+                        running_loss = 0
+
+            metrics = {'avg_loss': np.mean(total_loss),
+                       'max_loss': np.max(total_loss),
+                       'min_loss': np.min(total_loss)}
+            writer.add_hparams(hparams, metrics, run_name=run)
+            print("Training complete.")
